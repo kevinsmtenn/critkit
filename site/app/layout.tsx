593d29972d29b992bdfe26a6bc68b9d9005d1026
Kevin Tenn <kevinsmtenn@gmail.com>
2026-05-20 21:04:04 -0700

    fixed dependency issues

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata, Viewport } from "next"
 import { Geist, JetBrains_Mono } from "next/font/google"
+import { Analytics } from "@vercel/analytics/next"
 
 import "./globals.css"
 import { cn } from "@/lib/utils"
@@ -77,6 +78,7 @@ export default function RootLayout({
           {children}
           <CritKitProvider />
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
